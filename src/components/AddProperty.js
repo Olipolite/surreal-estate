c2009fd31faded1cb3f8f1fe0ecabf5c07d417cd
Olipolite <oliver.oquist3d@gmail.com>
2023-02-18 15:19:50 +0000

    added props in AddProperty component.

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/addproperty.css";
 
 const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+
   return (
     <div className="add-property">
+      <form onSubmit={handleAddProperty}>
+        <button type="submit">Submit</button>
+      </form>
       <h2 className="add-property-heading">Add Property</h2>
     </div>
   );
