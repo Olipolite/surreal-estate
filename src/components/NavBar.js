aa44bef396d3df3127d1950b8dbb7d694a139457
Olipolite <oliver.oquist3d@gmail.com>
2023-02-18 10:52:01 +0000

    Added Routes, can now swap between pages.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
-
+import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 
 const NavBar = () => {
@@ -11,10 +11,10 @@ const NavBar = () => {
       <h1>Surreal Estate </h1>
       <ul className="navbar-links">
         <li className="navbar-links-items">
-          <a href="url">View Properties</a>
+          <Link to="/">View Properties</Link>
         </li>
         <li className="navbar-links-items">
-          <a href="url">Add a Property</a>
+          <Link to="/add-property">Add a Property</Link>
         </li>
       </ul>
     </div>
---
+++ src/components/Properties.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Properties = () => {
+  return (
+    <div className="properties">
+      <h2 className="properties-heading">Properties</h2>
+    </div>
+  );
+};
+
+export default Properties;
