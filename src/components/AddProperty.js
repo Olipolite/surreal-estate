96b01cbbf13750eb024898736dce3e66d8c666d8
Olipolite <oliver.oquist3d@gmail.com>
2023-02-19 20:57:10 +0000

    added basic styling to fieldset, input and select forms

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -6,10 +6,11 @@ const AddProperty = () => {
     fields: {
       title: "",
       city: "Manchester",
-      types: "type",
+      type: "Flat",
       bathrooms: "1",
       bedrooms: "1",
       price: "0",
+      email: "",
     },
   };
 
@@ -27,96 +28,114 @@ const AddProperty = () => {
   return (
     <div className="add-property">
       <h2 className="add-property-heading">Add Property</h2>
-      <form onSubmit={handleAddProperty}>
-        <label htmlFor="title">
-          Title
-          <input
-            id="title"
-            name="title"
-            value={fields.title}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-        <label htmlFor="city">
-          City
-          <select
-            id="city"
-            name="city"
-            value={fields.city}
-            onChange={handleFieldChange}
-          >
-            <option value="Manchester">Manchester</option>
-            <option value="Leeds">Leeds</option>
-            <option value="Sheffield">Sheffield</option>
-            <option value="Liverpool">Liverpool</option>
-          </select>
-        </label>
-        <label htmlFor="type">
-          Type
-          <select
-            id="type"
-            name="type"
-            value={fields.type}
-            onChange={handleFieldChange}
-          >
-            <option value="Flat">Flat</option>
-            <option value="Detached">Detached</option>
-            <option value="Semi-Detached">Semi-Detached</option>
-            <option value="Terraced">Terraced</option>
-            <option value="End of Terrace">End of Terrace</option>
-            <option value="Cottage">Cottage</option>
-            <option value="Bungalow">Bungalow</option>
-          </select>
-        </label>
-        <label htmlFor="No. of Bathrooms">
-          No. of bathrooms
-          <select
-            id="Bathrooms"
-            name="Bathrooms"
-            value={fields.bathrooms}
-            onChange={handleFieldChange}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </label>
-        <label htmlFor="No. of Bedrooms">
-          No. of bedrooms
-          <select
-            id="Bedrooms"
-            name="Bedrooms"
-            value={fields.bedrooms}
-            onChange={handleFieldChange}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </label>
-        <label htmlFor="Price range(£)">
-          Price range(£)
-          <select
-            id="Price range(£)"
-            name="Price range(£)"
-            value={fields.price}
-            onChange={handleFieldChange}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </label>
-      </div>
+      <fieldset>
+        <div className="forms">
+          <label className="title" htmlFor="title">
+            Title
+            <input
+              id="title"
+              name="title"
+              value={fields.title}
+              onChange={handleFieldChange}
+            />
+          </label>
+          <div className="city">
+            <label htmlFor="city">
+              City
+              <select
+                id="city"
+                name="city"
+                value={fields.city}
+                onChange={handleFieldChange}
+              >
+                <option value="Manchester">Manchester</option>
+                <option value="Leeds">Leeds</option>
+                <option value="Sheffield">Sheffield</option>
+                <option value="Liverpool">Liverpool</option>
+              </select>
+            </label>
+          </div>
+          <div className="type">
+            <label htmlFor="type">
+              Type
+              <select
+                id="type"
+                name="type"
+                value={fields.type}
+                onChange={handleFieldChange}
+              >
+                <option value="Flat">Flat</option>
+                <option value="Detached">Detached</option>
+                <option value="Semi-Detached">Semi-Detached</option>
+                <option value="Terraced">Terraced</option>
+                <option value="End of Terrace">End of Terrace</option>
+                <option value="Cottage">Cottage</option>
+                <option value="Bungalow">Bungalow</option>
+              </select>
+            </label>
+          </div>
+          <div className="bathroom">
+            <label htmlFor="No. of Bathrooms">
+              No. of bathrooms
+              <select
+                id="bathrooms"
+                name="bathrooms"
+                value={fields.bathrooms}
+                onChange={handleFieldChange}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </label>
+          </div>
+          <div className="bedroom">
+            <label htmlFor="No. of Bedrooms">
+              No. of bedrooms
+              <select
+                id="bedrooms"
+                name="bedrooms"
+                value={fields.bedrooms}
+                onChange={handleFieldChange}
+              >
+                <option value="Studio">Studio</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </label>
+          </div>
+          <div className="price">
+            <label htmlFor="Price(£)">
+              Price(£)
+              <input
+                id="price"
+                name="price"
+                value={fields.price}
+                onChange={handleFieldChange}
+              />
+            </label>
+          </div>
+          <div className="email">
+            <form onSubmit={handleAddProperty}>
+              <label htmlFor="email">
+                E-mail
+                <input
+                  id="email"
+                  name="email"
+                  value={fields.email}
+                  onChange={handleFieldChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      </fieldset>
     </div>
   );
 };
