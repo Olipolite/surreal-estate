34a7820b5709eb56519613f7cef6799b05821597
Olipolite <oliver.oquist3d@gmail.com>
2023-02-24 00:12:47 +0000

    making property card component and tests

---
+++ src/tests/PropertyCard.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import PropertyCard from "../components/PropertyCard";
+
+describe("PropertyCard", () => {
+  const validProps = {
+    title: "test title",
+    type: "test type",
+    bathrooms: 1,
+    bedrooms: 1,
+    price: 9999,
+    city: "test city",
+    email: "test email",
+  };
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <PropertyCard
+        title={validProps.title}
+        type={validProps.type}
+        bathrooms={validProps.bathrooms}
+        bedrooms={validProps.bedrooms}
+        price={validProps.price}
+        city={validProps.city}
+        email={validProps.email}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
